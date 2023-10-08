342e3bc5750932c5a55950824c7c30e233c77adf
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-07 22:53:52 -0300

    Server running

--- server/index.js
+++ server/index.js
@@ -5,6 +5,34 @@ import http from "http";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-
-//Configuracion Moongose:
+import dotenv from "dotenv";
+dotenv.config();
+//Moongose configuration:
 const url = process.env.MONGODB_URL;
+
+mongoose.Promise = global.Promise;
+
+const app = express();
+const PORT = 4000;
+
+// http module server and socket server;
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+//MiddleWare:
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//database conexion.
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+  console.log("Database Connected");
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
