9eb5f20265cca1953304863a69189a835f01726d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-08 10:03:21 -0300

    React Chat client Starting

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mt-3">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="text-center">CHAT</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
